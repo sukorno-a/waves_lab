6f780d26515db07d969cdecf9091c71eaa200c91
guavacadofilms <118949305+guavacadofilms@users.noreply.github.com>
2023-01-03 18:37:04 +0000

    Formatting
    Neatened code and added comments

--- fourier.py
+++ fourier.py
@@ -23,6 +23,7 @@ def square(x, n, period, amp):
     T : float
         Output of function"""
     
+    # create array of n terms to sum together up to the defined upper limit N
     n_array = np.linspace(1, n, int((n + 1) / 2))
     L = period / 2
     a_0 = amp / 2
@@ -38,10 +39,10 @@ def square(x, n, period, amp):
 x = np.linspace(0, 240, 1000)
 period = 240
 amplitude = 100
+N = 5 # number of terms, must be odd; change based on desired precision of Fourier series
+N_array = np.linspace(1, N, int((N + 1) / 2)) # create array of odd numbers from 1 to N
 
 plt.figure()
-N = 5
-N_array = np.linspace(1, N, int((N + 1) / 2))
 for N in N_array:
     plt.plot(x, square(x, N, period, amplitude), label = int(N))
     plt.legend(title = "Terms:")
@@ -49,6 +50,6 @@ plt.xlabel("t")
 plt.ylabel("T(t)")
 plt.show()
 
-# with enough terms (N ~ 100), the plot closely resembles a square wave as we would expect from Fourier theory
+# with enough terms (N ~ 101), the plot closely resembles a square wave as we would expect from Fourier theory
 # this shows that the Fourier series for this square wave is correct
 # the Gibb's phenomena at the boundaries occur due to x being undefined at the edge of the waves
