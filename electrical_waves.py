e23ab0fea48b4e648697667c7ea2c6e77d709abe
guavacadofilms <118949305+guavacadofilms@users.noreply.github.com>
2023-02-02 15:32:17 +0000

    task 3.8a
    plotting w against k, to see the dispersion relation

--- electrical_waves.py
+++ electrical_waves.py
@@ -1,5 +1,10 @@
 import uncertainties
 from uncertainties import ufloat
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("darkgrid")
 
 # task 3.5b - calculate Vphase and its uncertainty using values of L = 330 + 20% μH and C = 0.015 + 10% μF/section
 
@@ -12,5 +17,18 @@ sec_per_sec = v_phase(L, C)
 sec_per_micros = (sec_per_sec)*1e-6
 print(f"Phase velocity is {v_phase(L, C)} sections per second, or {sec_per_micros} sections per microsecond")
 
+# task 3.8a - plot dispersion relation of the line, w against k
+data = np.loadtxt("PART3_Electrical_Waves/task_3.7/amplitude_frequency.csv", delimiter=",", skiprows = 1, usecols = [0, 2, 3, 4, 5, 6])
+frequencies = data[:,0]
+amp_ratio = data[:,5]
+
+k = []
+plt.figure()
+for i in range(len(frequencies)):
+    k.append(i + 1)
+plt.plot(frequencies, k)
+plt.xlabel("Frequency/kHz")
+plt.ylabel("Wavenumber")
+plt.show()
 
 
